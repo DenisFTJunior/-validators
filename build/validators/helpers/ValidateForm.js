6e4c6fcfd027d9cc9f166c8dcf5e6f16d8a3af2f
Denis Junior <junior.denis2354@outlook.com>
2022-01-17 14:09:53 -0300

    clean error add to validateForm

--- build/validators/helpers/ValidateForm.js
+++ build/validators/helpers/ValidateForm.js
@@ -1,22 +1,55 @@
 "use strict";
 exports.__esModule = true;
-var ErrorMsg_1 = require("../../components/ErrorMsg");
+var ErrorMsg_1 = require("../../../src/components/ErrorMsg");
 var ValidateForm = function (_a) {
-    var x = _a.x, config = _a.config;
-    return ({
-        get: function () { return x; },
-        set: function (_a) {
-            var _b, _c, _d, _e, _f;
-            var f = _a.f, value = _a.value, refs = _a.refs;
-            var _g = f(value, config), result = _g.result, msg = _g.msg;
-            if (!result)
-                (0, ErrorMsg_1.ErrorMsg)(refs, config ? config : {}, msg);
-            if (((_b = refs === null || refs === void 0 ? void 0 : refs.style) === null || _b === void 0 ? void 0 : _b.borderColor) && ((_c = refs === null || refs === void 0 ? void 0 : refs.style) === null || _c === void 0 ? void 0 : _c.borderColor) === (((_d = config === null || config === void 0 ? void 0 : config.warning) === null || _d === void 0 ? void 0 : _d.color) || "#ff726f")) {
-                refs.style.borderColor = ((_e = config === null || config === void 0 ? void 0 : config.style) === null || _e === void 0 ? void 0 : _e.defaultBorderColor) || "#c4c4c4";
-                refs.style.color = ((_f = config === null || config === void 0 ? void 0 : config.style) === null || _f === void 0 ? void 0 : _f.defaultColor) || "#c4c4c4";
-            }
-            return ValidateForm({ x: x || result });
-        }
-    });
+  var x = _a.x,
+    config = _a.config;
+  return {
+    get: function () {
+      return x;
+    },
+    set: function (_a) {
+      var f = _a.f,
+        value = _a.value,
+        refs = _a.refs;
+      var _b = f(value, config),
+        result = _b.result,
+        msg = _b.msg;
+      if (!result) (0, ErrorMsg_1.ErrorMsg)(refs, config ? config : {}, msg);
+      clean(refs, config);
+      return ValidateForm({ x: x || result });
+    },
+  };
+};
+var clean = function (refs, config) {
+  var _a, _b, _c, _d, _e;
+  var child = document.querySelector("#error-fast-validators-handler");
+  if (child) refs.removeChild(child);
+  if (
+    ((_a = refs === null || refs === void 0 ? void 0 : refs.style) === null ||
+    _a === void 0
+      ? void 0
+      : _a.borderColor) &&
+    ((_b = refs === null || refs === void 0 ? void 0 : refs.style) === null ||
+    _b === void 0
+      ? void 0
+      : _b.borderColor) ===
+      (((_c =
+        config === null || config === void 0 ? void 0 : config.warning) ===
+        null || _c === void 0
+        ? void 0
+        : _c.color) || "#ff726f")
+  ) {
+    refs.style.borderColor =
+      ((_d = config === null || config === void 0 ? void 0 : config.style) ===
+        null || _d === void 0
+        ? void 0
+        : _d.defaultBorderColor) || "#c4c4c4";
+    refs.style.color =
+      ((_e = config === null || config === void 0 ? void 0 : config.style) ===
+        null || _e === void 0
+        ? void 0
+        : _e.defaultColor) || "#c4c4c4";
+  }
 };
 exports["default"] = ValidateForm;
--- src/validators/helpers/ValidateForm.ts
+++ src/validators/helpers/ValidateForm.ts
@@ -4,6 +4,7 @@ import {
   ValidateFormSchema,
 } from "../../schema/ValidateForm";
 import { ErrorMsg } from "../../components/ErrorMsg";
+import { Config } from "../../schema/config/Config";
 
 const ValidateForm = ({
   x,
@@ -13,13 +14,21 @@ const ValidateForm = ({
   set: ({ f, value, refs }: ValidateFormObject) => {
     const { result, msg } = f(value, config);
     if (!result) ErrorMsg(refs, config ? config : {}, msg);
-    if (refs?.style?.borderColor && refs?.style?.borderColor === (config?.warning?.color || "#ff726f")) {
-      refs.style.borderColor = config?.style?.defaultBorderColor || "#c4c4c4";
-      refs.style.color = config?.style?.defaultColor || "#c4c4c4";
-    }
-
+    clean(refs, config)
     return ValidateForm({ x: x || result });
   },
 });
+
+const clean = (refs: HTMLElement, config?: Config) => {
+  const child = document.querySelector("#error-fast-validators-handler");
+  if (child) refs.removeChild(child);
+  if (
+    refs?.style?.borderColor &&
+    refs?.style?.borderColor === (config?.warning?.color || "#ff726f")
+  ) {
+    refs.style.borderColor = config?.style?.defaultBorderColor || "#c4c4c4";
+    refs.style.color = config?.style?.defaultColor || "#c4c4c4";
+  }
+};
 
 export default ValidateForm;
